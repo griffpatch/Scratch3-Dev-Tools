f52732ade83f5668fcf134603021b330d7d8cac4
griffpatch <andy@griffpatch.co.uk>
2020-07-07 20:58:40 +0100

    v0.2.4 - Added Much Stuff - Fixed much stuff We now have more integration with Scratch Ctrl + Space to insert new blocks Middle click to jump to definitions Clean up + on the stage menu

--- injectSimple.js
+++ injectSimple.js
@@ -5,3 +5,14 @@ s.onload = function() {
     this.remove();
 };
 (document.head || document.documentElement).appendChild(s);
+
+/*
+const s2 = document.createElement('script');
+// TODO: add "math.js" to web_accessible_resources in manifest.json
+s2.src = chrome.runtime.getURL('math.js');
+s2.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(s2);
+*/
+

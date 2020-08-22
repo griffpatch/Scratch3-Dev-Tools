242b3fef616ff1e2bd43e694df289d26aeb54165
Joe Clinton <joeclinton1@btinternet.com>
2020-08-22 22:30:11 +0100

    changed to raw url

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         if( details.url == "https://scratch.mit.edu/js/projects.bundle.js" )
-            return {redirectUrl: "https://github.com/Joeclinton1/Scratch3-Dev-Tools/blob/master/projects.bundle.js" };
+            return {redirectUrl: "https://raw.githubusercontent.com/Joeclinton1/Scratch3-Dev-Tools/master/projects.bundle.js" };
     },
     {urls: ["*://scratch.mit.edu/*.js"]},
     ["blocking"]

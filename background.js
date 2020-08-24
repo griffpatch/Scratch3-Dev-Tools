7c14bc9767be2b9b882309e3e9f36cef57e7eebf
Joe Clinton <joeclinton1@btinternet.com>
2020-08-24 15:57:53 +0100

    Added comment to background.js

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// Listen for requests and if the request is for projects.bundle.js then block it
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         if( details.url == "https://scratch.mit.edu/js/projects.bundle.js" ){
